f023bc76b5c80b9ff50dadf37b6c853ae22dc455
Victor <victorchike247@gmail.com>
2024-09-10 19:00:19 +0100

    added login-register page

---
+++ lib/pages/login_register_page.dart
@@ -0,0 +1,45 @@
+import 'package:flippy_app/components/components.dart';
+import 'package:flutter/material.dart';
+
+class LoginRegisterPage extends StatelessWidget {
+  const LoginRegisterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Choose Location",
+            style: TextStyle(color: Colors.white, fontSize: 14)),
+        backgroundColor: const Color(0xFF273773),
+        actions: [
+          MaterialButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/login');
+              },
+              child: const Text("Login", style: TextStyle(color: Colors.white)))
+        ],
+      ),
+      body: Center(
+        child: Container(
+          color: const Color(0xFF273773),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const AppHeader(alternate: true),
+              const SizedBox(height: 50),
+              Image.asset("assets/main_avatar.png"),
+              const SizedBox(height: 50),
+              AppButton(
+                  title: "Login With Google", onTap: () {}, secondary: true),
+              const SizedBox(height: 20),
+              AppButton(
+                  title: "Continue With Facebook",
+                  onTap: () {},
+                  secondary: true),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
